71d1376132fb2bf45e84585ca804b22e12713252
webmancer1 <mwendwajames2004@gmail.com>
2026-01-09 01:11:35 +0300

    theme changes

--- app/src/main/java/com/example/memora/ui/theme/Theme.kt
+++ app/src/main/java/com/example/memora/ui/theme/Theme.kt
@@ -12,15 +12,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = SlateBlue80,
+    secondary = MutedGold80,
+    tertiary = SoftTaupe80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = SlateBlue40,
+    secondary = MutedGold40,
+    tertiary = SoftTaupe40
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -37,7 +37,7 @@ private val LightColorScheme = lightColorScheme(
 fun MemoraTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
