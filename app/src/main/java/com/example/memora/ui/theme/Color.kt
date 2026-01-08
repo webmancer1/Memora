c38e8221cd6b4b3bc7f03e344d91bf68fe9d68ab
webmancer1 <mwendwajames2004@gmail.com>
2026-01-09 01:10:58 +0300

    UI changes

--- app/src/main/java/com/example/memora/ui/theme/Color.kt
+++ app/src/main/java/com/example/memora/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.example.memora.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val SlateBlue80 = Color(0xFFB0BEC5)
+val MutedGold80 = Color(0xFFD7CCC8)
+val SoftTaupe80 = Color(0xFFEFEBE9)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val SlateBlue40 = Color(0xFF455A64)
+val MutedGold40 = Color(0xFF8D6E63)
+val SoftTaupe40 = Color(0xFF795548)
