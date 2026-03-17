003517d1e494fedd3c4641e71ca46d1f656f7c19
webmancer1 <mwendwajames2004@gmail.com>
2026-03-17 07:57:10 +0300

    UI updates

--- app/src/main/java/com/example/memora/ui/theme/Color.kt
+++ app/src/main/java/com/example/memora/ui/theme/Color.kt
@@ -2,10 +2,14 @@ package com.example.memora.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val SlateBlue80 = Color(0xFFB0BEC5)
-val MutedGold80 = Color(0xFFD7CCC8)
-val SoftTaupe80 = Color(0xFFEFEBE9)
+val DeepNavy = Color(0xFF0F172A)
+val Navy80 = Color(0xFF1E293B)
+val SlateGray = Color(0xFF334155)
+val LightSlate = Color(0xFF94A3B8)
 
-val SlateBlue40 = Color(0xFF455A64)
-val MutedGold40 = Color(0xFF8D6E63)
-val SoftTaupe40 = Color(0xFF795548)
+val SoftGold = Color(0xFFD4AF37)
+val PaleGold = Color(0xFFFDE047)
+val SoftEmerald = Color(0xFF10B981)
+
+val AppBackgroundLight = Color(0xFFF8FAFC)
+val AppSurfaceLight = Color.White
