8aff136a303d99933e91e28e908d851cc70fef30
webmancer1 <mwendwajames2004@gmail.com>
2026-03-28 21:55:30 +0300

    changes

--- app/src/main/java/com/example/memora/data/local/entity/TaskEntity.kt
+++ app/src/main/java/com/example/memora/data/local/entity/TaskEntity.kt
@@ -10,7 +10,7 @@ data class TaskEntity(
     val title: String,
     val description: String,
     val categoryId: Long,
-    val priority: Int, // 0 = Low, 1 = Medium, 2 = High
+    val priority: Int, 
     val dueDate: Long?,
     val isCompleted: Boolean,
     val createdDate: Long
