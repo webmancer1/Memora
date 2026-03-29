6258148cf8aa89b0cbd0df8ae44f3d217883642f
webmancer1 <mwendwajames2004@gmail.com>
2026-03-30 01:23:26 +0300

    changes

--- app/src/main/java/com/example/memora/ui/theme/Theme.kt
+++ app/src/main/java/com/example/memora/ui/theme/Theme.kt
@@ -41,7 +41,7 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun MemoraTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
+    
     dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
